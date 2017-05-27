b53729238ba475ef1338b93552b1c62f5a1b97cc
Volodarskiy Nikita <volodarskyns@gmail.com>
2017-05-27 18:05:29 +0300

    Demand&Supply Graphs

---
+++ my-project/src/components/Hello.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+  <head>
+  </head>
+    <h1>Graphs of economics</h1>
+        <div id="result">
+          <h1></h1>
+        </div>
+        <div style="width: 400px">
+        <canvas id="demandSupplyChart" width="400" height="400"></canvas>
+        <p>Demand and Supply</p>
+        </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-weight: normal;
+  margin-left: 35%;
+}
+
+p {
+  font-weight: normal;
+  margin-left: 26%;
+  margin-top: -20px;
+}
+</style>
---
+++ my-project/src/main.js
@@ -0,0 +1,45 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import BootstrapVue from 'bootstrap-vue';
+import './lib/jquery.min.js'
+import  '../node_modules/chart.js/dist/Chart.min.js'
+
+Vue.use(BootstrapVue);
+
+import router from './router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
+
+//drawing demand/supply chart
+var ctx = document.getElementById("demandSupplyChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+   data: {
+      datasets: [{
+      label: "Demand",
+      data: [{x:2, y:20}, {x:2, y:2}], //demand info
+      fill: false,
+      borderColor: '#000000',
+    }, {
+        label: "Supply",
+        data: [{x:20, y:2}, {x:20, y:20}], //supply info
+        fill: false,
+        borderColor: '#f49242',
+      }],
+   },
+    options: {          
+      showLines: true, // disable for all datasets    
+        }
+    });
